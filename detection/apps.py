52da73434773e4d823cb14a3cdec3a99b9359963
Zunaira Fatima <145633319+Zunairafatima9@users.noreply.github.com>
2025-05-25 15:03:19 -0700

    Update apps.py

--- detection/apps.py
+++ detection/apps.py
@@ -4,3 +4,5 @@ from django.apps import AppConfig
 class DetectionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'detection'
+if __name__ == '__main__':
+    app.run(debug=False, host='0.0.0.0')
